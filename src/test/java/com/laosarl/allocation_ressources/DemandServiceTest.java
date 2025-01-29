e7ead830c49b92dc6331cae087b955a42b8e3b6c
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2025-01-29 10:51:15 +0100

    code completion.

--- src/test/java/com/laosarl/allocation_ressources/DemandServiceTest.java
+++ src/test/java/com/laosarl/allocation_ressources/DemandServiceTest.java
@@ -2,7 +2,6 @@ package com.laosarl.allocation_ressources;
 
 import com.laosarl.allocation_ressources.domain.Demand;
 import com.laosarl.allocation_ressources.model.CreateDemandRequestDTO;
-import com.laosarl.allocation_ressources.model.DemandDTO;
 import com.laosarl.allocation_ressources.repository.DemandRepository;
 import com.laosarl.allocation_ressources.service.DemandService;
 import com.laosarl.allocation_ressources.service.mapper.DemandMapper;
@@ -14,10 +13,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class DemandServiceTest {
@@ -29,7 +29,17 @@ public class DemandServiceTest {
     DemandService objectUnderTest;
 
     @Test
-    void createDemand_WhenAllConditionsAreMet_ShouldCreateTheDemand(){
+    void createDemand_WhenAllConditionsAreMet_ShouldCreateTheDemand() {
 
+        //Given
+        CreateDemandRequestDTO request = new CreateDemandRequestDTO().resourceName("stylo").quantity("1").description("stylo à bille").justification("Pour écrire").urgency("urgent").dueDate("2024/05/05");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate formattedDueDate = LocalDate.parse(request.getDueDate(), formatter);
+        LocalDateTime formattedDateTime = LocalDateTime.now();
+
+        //When
+        objectUnderTest.createDemand(request);
+        //Then
+        verify(demandRepository).save(argThat(demand -> demand.getResourceName().equals(request.getResourceName()) && demand.getDescription().equals(request.getDescription()) && demand.getJustification().equals(request.getJustification()) && demand.getDueDate().equals(formattedDueDate)));
     }
 }
