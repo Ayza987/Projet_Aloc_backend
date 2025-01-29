1f8bc6a115abb73c7906898243afc649b12a107f
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2025-01-29 15:12:35 +0100

    code completion.

---
+++ src/test/java/com/laosarl/allocation_ressources/DemandMapperTest.java
@@ -0,0 +1,33 @@
+package com.laosarl.allocation_ressources;
+
+
+import com.laosarl.allocation_ressources.domain.Demand;
+import com.laosarl.allocation_ressources.model.DemandDTO;
+import com.laosarl.allocation_ressources.service.mapper.DemandMapper;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DemandMapperTest {
+
+    private final DemandMapper demandMapper = Mappers.getMapper(DemandMapper.class);
+
+    @Test
+    void toDemandDTO_ShouldMapDemandCorrectly() {
+        //Given
+        Demand demandToMap = Demand.builder().id(1L).resourceName("azert").userName("azer").userEmail("azerty").description("sdfg").justification("azertyu").quantity("azertyu").urgency("urgent").status("PENDING").build();
+        //When
+        DemandDTO dto = demandMapper.toDemandDTO(demandToMap);
+        //Then
+        assertNotNull(dto);
+        assertEquals(demandToMap.getResourceName(), dto.getResourceName());
+        assertEquals(demandToMap.getUserName(), dto.getUserName());
+        assertEquals(demandToMap.getDescription(), dto.getDescription());
+        assertEquals(demandToMap.getJustification(), dto.getJustification());
+        assertEquals(demandToMap.getQuantity(), dto.getQuantity());
+    }
+
+
+}
